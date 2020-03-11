b64a5011a1871bb5e70f1cfd52b917568941f21f
sonic6 <alshihabi.ahmad@gmail.com>
2020-03-11 20:25:09 +0100

    Added navmesh
    Working on setting up Ai agents

---
+++ Queer Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Camera myCam;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("");
+    }
+}
