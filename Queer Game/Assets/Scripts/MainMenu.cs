746a3c31a54a29b33f4195fba99b95d5174ea9c7
sonic6 <alshihabi.ahmad@gmail.com>
2020-05-30 02:18:02 +0200

    Created new main menu scene
    programmed camera rotation for main manu and a fading effect of title screen and text

---
+++ Queer Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [Tooltip("This image will be the first thing the player sees in the main menu. After pressing a button, it fades away")]
+    [SerializeField] Image startImage;
+
+    [SerializeField] TMP_Text myText;
+
+    Animator CameraAnimator;
+
+    //Becomes true on the very first button or mouse click the player clicks
+    bool fadeStart = false;
+
+    private void Start()
+    {
+        CameraAnimator = Camera.main.GetComponent<Animator>();
+        StartCoroutine(FadeInOut());
+    }
+
+    void Update()
+    {
+        if(startImage.gameObject.activeSelf)
+        {
+            if (Input.anyKeyDown)
+            {
+                fadeStart = true;
+            }
+            
+            if (fadeStart)
+            {
+                myText.gameObject.SetActive(false);
+                Color main = startImage.color;
+                startImage.color = new Color(main.r, main.g, main.b, main.a - 1 * Time.deltaTime);
+                if (startImage.color.a <= 0)
+                {
+                    startImage.gameObject.SetActive(false);
+                    CameraAnimator.SetTrigger("turn");
+                }
+            }
+        }
+    }
+
+    IEnumerator FadeInOut()
+    {
+        while(myText.gameObject.activeSelf)
+        {
+            if (myText.gameObject.activeSelf == false)
+                break;
+
+            var og = myText.color.a;
+            while (myText.color.a > 0)
+            {
+                myText.color = new Color(myText.color.r, myText.color.g, myText.color.b, myText.color.a - 1 * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+            while(myText.color.a < og)
+            {
+                myText.color = new Color(myText.color.r, myText.color.g, myText.color.b, myText.color.a + 1 * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+            
+        }
+        yield break;
+    }
+}
